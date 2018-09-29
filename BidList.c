14f532f619bf171d975d7d0dc72fd2d40c2bc4da
Eric Vallsmadella <eric@MacBook-MacBook-Pro-de-Eric.local>
2018-09-29 18:40:32 +0200

    first commit

---
+++ BidList.c
@@ -0,0 +1,48 @@
+/****************************************************************************************************************************************************
+ * FUNCTIONALITY: Giving the user all the functionalities for creating, modifying and destroying a Bidirectional linked list with a point of interest
+ * AUTHOR: Eric Vallsmadella
+ * CREATION DATE: 6/05/2018
+ * LAST MODIFICATION: 10/05/2018
+ ***************************************************************************************************************************************************/
+#include "BidList.h"
+
+BidList  BidList_Create(){
+
+}
+void  BidList_InsertFoward(){
+
+}
+void  BidList_InsertBefore(){
+
+}
+int  BidList_Check(){
+
+}
+void  BidList_Delete(){
+
+}
+void  BidList_Forward(){
+
+}
+void  BidList_Backwards(){
+
+}
+void BidList_GoBeginning(){
+
+}
+void BidList_GoEnd(){
+
+}
+int BidList_End(){
+
+}
+int BidList_Empty(){
+
+}
+void BidList_Destroy(){
+
+}
+
+
+
+
